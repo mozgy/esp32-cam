7e8a3de16b2f3ddba561dcc7511554c5b10cdfb7
Mario Mikočević <mozgy@t-com.hr>
2024-05-23 18:52:42 +0200

    move initOTA to WiFi code block

--- src/MozzCam.cpp
+++ src/MozzCam.cpp
@@ -106,62 +106,6 @@ void fnElapsedStr( char *str ) {
 
 }
 
-void initOTA( void ) {
-
-  // Port defaults to 3232
-  // ArduinoOTA.setPort( 3232 );
-
-  // Hostname defaults to esp3232-[MAC]
-  String hostName;
-  hostName = ESP_CAM_HOSTNAME;
-  hostName += "-";
-  hostName += CAM_SERIAL;
-  ArduinoOTA.setHostname( hostName.c_str() );
-
-  // No authentication by default
-  // ArduinoOTA.setPassword( "admin" );
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash( "21232f297a57a5a743894a0e4a801fc3" );
-
-  ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
-
-      // Stop the camera since OTA will crash the module if it is running.
-      // the unit will need rebooting to restart it, either by OTA on success, or manually by the user
-      log_d( "Stopping Camera" );
-      esp_err_t err = esp_camera_deinit();
-      // critERR = "<h1>OTA Has been started</h1><hr><p>Camera has Halted!</p>";
-      // critERR += "<p>Wait for OTA to finish and reboot, or <a href=\"control?var=reboot&val=0\" title=\"Reboot Now (may interrupt OTA)\">reboot manually</a> to recover</p>";
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-
-  ArduinoOTA.begin();
-
-}
-
 void getNTPTime( void ) {
 
   log_i( "Contacting Time Server - " );
--- src/wifi.cpp
+++ src/wifi.cpp
@@ -1,4 +1,7 @@
 
+#include <ArduinoOTA.h>
+#include "esp_camera.h"
+
 #include "camera_model.h"
 #include "mywifi.h"
 #include "credentials.h"
@@ -64,6 +67,62 @@ void WiFiStationDisconnected( WiFiEvent_t event, WiFiEventInfo_t info ) {
 
 }
 
+void initOTA( void ) {
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort( 3232 );
+
+  // Hostname defaults to esp3232-[MAC]
+  String hostName;
+  hostName = ESP_CAM_HOSTNAME;
+  hostName += "-";
+  hostName += CAM_SERIAL;
+  ArduinoOTA.setHostname( hostName.c_str() );
+
+  // No authentication by default
+  // ArduinoOTA.setPassword( "admin" );
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash( "21232f297a57a5a743894a0e4a801fc3" );
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // Stop the camera since OTA will crash the module if it is running.
+      // the unit will need rebooting to restart it, either by OTA on success, or manually by the user
+      log_d( "Stopping Camera" );
+      esp_err_t err = esp_camera_deinit();
+      // critERR = "<h1>OTA Has been started</h1><hr><p>Camera has Halted!</p>";
+      // critERR += "<p>Wait for OTA to finish and reboot, or <a href=\"control?var=reboot&val=0\" title=\"Reboot Now (may interrupt OTA)\">reboot manually</a> to recover</p>";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+}
+
 void initWiFi( void ) {
 
   wifiSTATries = 1;
@@ -94,17 +153,3 @@ void initWiFi( void ) {
 #endif
 
 }
-
-// SDK definitions
-// typedef enum {
-//     WIFI_AUTH_OPEN = 0,         /**< authenticate mode : open */
-//      WIFI_AUTH_WEP,              /**< authenticate mode : WEP */
-//      WIFI_AUTH_WPA_PSK,          /**< authenticate mode : WPA_PSK */
-//      WIFI_AUTH_WPA2_PSK,         /**< authenticate mode : WPA2_PSK */
-//      WIFI_AUTH_WPA_WPA2_PSK,     /**< authenticate mode : WPA_WPA2_PSK */
-//      WIFI_AUTH_WPA2_ENTERPRISE,  /**< authenticate mode : WPA2_ENTERPRISE */
-//      WIFI_AUTH_WPA3_PSK,         /**< authenticate mode : WPA3_PSK */
-//      WIFI_AUTH_WPA2_WPA3_PSK,    /**< authenticate mode : WPA2_WPA3_PSK */
-//      WIFI_AUTH_WAPI_PSK,         /**< authenticate mode : WAPI_PSK */
-//      WIFI_AUTH_MAX
-// } wifi_auth_mode_t;
