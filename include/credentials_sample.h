ba6cfb98cd20be35c1c0bc562c6361e65b7e06f6
Mario Mikočević <mozgy@t-com.hr>
2024-07-10 23:32:44 +0200

    variable name change for better clarity

--- include/credentials_sample.h
+++ include/credentials_sample.h
@@ -1,8 +1,8 @@
 #ifndef _CREDENTIALS_H_
 #define _CREDENTIALS_H_
 
-static const char* ssid     = "****";
-static const char* password = "****";
+static const char* wifi_ssid     = "****";
+static const char* wifi_password = "****";
 
 static const char* http_username = "admin";
 static const char* http_password = "admin";
