3d5c49c5e1e4e6f27dcaa5d4c158034bda3cd2c5
Mario Mikočević <mozgy@t-com.hr>
2026-08-15 21:27:18 +0200

    add camera pin definition for Waveshare ESP32 S3 Cam

--- include/camera_pins.h
+++ include/camera_pins.h
@@ -339,6 +339,26 @@
 #define HREF_GPIO_NUM     42
 #define PCLK_GPIO_NUM     5
 
+#elif defined(CAMERA_MODEL_Waveshare_ESP32S3_CAM)
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 38
+#define SIOD_GPIO_NUM 8
+#define SIOC_GPIO_NUM 7
+
+#define Y2_GPIO_NUM 45
+#define Y3_GPIO_NUM 47
+#define Y4_GPIO_NUM 48
+#define Y5_GPIO_NUM 46
+#define Y6_GPIO_NUM 42
+#define Y7_GPIO_NUM 40
+#define Y8_GPIO_NUM 39
+#define Y9_GPIO_NUM 21
+
+#define VSYNC_GPIO_NUM 17
+#define HREF_GPIO_NUM 18
+#define PCLK_GPIO_NUM 41
+
 #else
 #error "Camera model not selected"
 #endif
